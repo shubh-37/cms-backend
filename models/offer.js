978e807b83a70617d62cea4c0fcf550c62cc6491
shubh-37 <shubh.arya37@gmail.com>
2023-04-09 00:55:13 +0530

    refact: removed perk schema and made model scalable

--- models/offer.js
+++ models/offer.js
@@ -1,13 +1,31 @@
 const Schema = require("mongoose").Schema;
 
-const PerkSchema = new Schema({
-  name: String,
-  iconUrl:  String,
-  description: String,
-  amount:  Number
-});
-
-const compDiversificationSchema = {
+const modelProperties = {
+  uniqueId: {
+    type: String,
+    unique: true,
+    index: true,
+  },
+  org: {
+    type: Schema.Types.ObjectId,
+    ref: "Organisation",
+  },
+  candidateName: {
+    type: String,
+  },
+  jobTitle: {
+    type: String,
+  },
+  jobLocation: {
+    type: String,
+  },
+  jobExpiryDate: {
+    type: Date,
+  },
+  currency: {
+    type: String,
+    default: "INR",
+  },
   baseSalary: {
     type: Number,
   },
@@ -21,58 +39,8 @@ const compDiversificationSchema = {
     type: Number,
   },
   benefits: {
-    type: Number,
+    type: [Schema.Types.ObjectId],
   },
-};
-
-const modelProperties = {
-  uniqueId: {
-    type: String,
-    unique: true,
-    index: true,
-  },
-  org: {
-    type: Schema.Types.ObjectId,
-    ref: "Organisation",
-  },
-  introSection: {
-    candidateName: {
-      type: String,
-    },
-    jobTitle: {
-      type: String,
-    },
-    jobLocation: {
-      type: String,
-    },
-    jobExpiryDate: {
-      type: Date,
-    },
-    currency: {
-      type: String,
-    },
-    baseSalary: {
-      type: Number,
-    },
-    signingBonus: {
-      type: Number,
-    },
-    noOfShares: {
-      type: Number,
-    },
-    targetBonus: {
-      type: Number,
-    },
-    benefits: {
-      type: Number,
-    },
-  },
-  benefitsSection: {
-    type: [PerkSchema]
-  },
-  compProjection: {
-    type: [compDiversificationSchema]
-  }
 };
 
 const modelOptions = {
