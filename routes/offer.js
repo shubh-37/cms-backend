beebd6dd3f75502a25b0b212f34622b97c92f298
shubh-37 <shubh.arya37@gmail.com>
2023-04-09 00:58:37 +0530

    added a post route to create an instance in offer model

--- routes/offer.js
+++ routes/offer.js
@@ -1,16 +1,25 @@
 var express = require("express");
 var router = express.Router();
 
-function offer(connection, Models) {
+function offer(Models) {
   /* GET users listing. */
   const { Offer } = Models;
-  console.log({ Offer });
+  console.log(Models);
   router.get("/", async function (req, res) {
     const { uniqueId } = req.query;
     console.log({ uniqueId });
     const offerInstance = await Offer.findOne({ uniqueId }).populate({ path: "org", model: "Organisation" });
     res.send({ offer: offerInstance });
   });
+  router.post("/create", async function (req, res){
+    const data = req.body;
+    try {
+      const emp = await Offer.create({ data });
+      res.status(201).json( { emp }); 
+    } catch (error) {
+      res.status(400).json({ error });
+    }
+  })
   return router;
 }
 
